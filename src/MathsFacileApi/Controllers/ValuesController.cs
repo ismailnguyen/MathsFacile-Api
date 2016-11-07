5fc53ead7ba87d5e9322b44d84ff77954612a271
ismailnguyen <me@nguyenismail.com>
2016-11-07 12:32:55 +0100

    Add rest api for calculs

---
+++ src/MathsFacileApi/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace MathsFacileApi.Controllers
+{
+    [Route("[controller]")]
+    public class CalculController : Controller
+    {
+        // GET calcul/5
+        [HttpGet("{value}")]
+        public object Get(string value)
+        {
+            return new DataTable().Compute(value, "");
+        }
+
+        // POST /calcul
+        [HttpPost]
+        public object Post([FromBody] string value)
+        {
+            return new DataTable().Compute(value, "");
+        }
+    }
+}
